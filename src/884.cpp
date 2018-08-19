bc5cbd2a15f010dac27506bf0b0f654648b9c747
Guofan Yin <guofan.yin@shopify.com>
2018-08-18 20:57:21 -0400

    Continue.

--- src/884.cpp
+++ src/884.cpp
@@ -1,44 +1,55 @@
 #include <algorithm>
 #include <iostream>
 #include <set>
+#include <sstream>
 #include <stdio.h>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 using namespace std;
+
 class Solution {
 public:
-  string decodeAtIndex(string S, int K) {
-    int n = S.size();
-    long size = 0;
-    string ans = "";
-    for (int i = 0; i < n; i++) {
-      if (isdigit(S[i])) {
-        size *= S[i] - '0';
-      } else {
-        size++;
-      }
+  vector<string> uncommonFromSentences(string A, string B) {
+    vector<string> t1;
+    vector<string> t2;
+
+    stringstream s1(A);
+    stringstream s2(B);
+    string tmp;
+    while (s1 >> tmp)
+      t1.push_back(tmp);
+    while (s2 >> tmp)
+      t2.push_back(tmp);
+
+    unordered_map<string, int> um1;
+    unordered_map<string, int> um2;
+
+    for (int i = 0; i < (int)t1.size(); i++) {
+      um1[t1[i]]++;
+    }
+    for (int i = 0; i < (int)t2.size(); i++) {
+      um2[t2[i]]++;
     }
 
-    for (int i = n - 1; i >= 0; i--) {
-      K %= size;
-      if (!K && !isdigit(S[i])) {
-        ans += S[i];
-        break;
-      }
-
-      if (isdigit(S[i]))
-        size /= S[i] - '0';
-      else
-        size--;
+    vector<string> ans;
+    for (auto it = um1.begin(); it != um1.end(); it++) {
+      if (it->second == 1 && um2.count(it->first) == 0)
+        ans.push_back(it->first);
+    }
+    for (auto it = um2.begin(); it != um2.end(); it++) {
+      if (it->second == 1 && um1.count(it->first) == 0)
+        ans.push_back(it->first);
     }
     return ans;
   }
 };
+
 int main() {
-  string S = "leet2code3";
-  int K = 10;
+  string A = "this apple is sweet";
+  string B = "this apple is sour";
   Solution solution = Solution();
-  cout << solution.decodeAtIndex(S, K);
+  solution.uncommonFromSentences(A, B);
 
   return 0;
 }
--- src/885.cpp
+++ src/885.cpp
@@ -1,39 +1,56 @@
 #include <algorithm>
 #include <iostream>
 #include <set>
+#include <sstream>
 #include <stdio.h>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
-  int numRescueBoats(vector<int> &people, int limit) {
-    int len = people.size();
-    int i = 0;
-    int j = len - 1;
-    int ans = 0;
-    sort(people.begin(), people.end());
+  vector<vector<int>> spiralMatrixIII(int R, int C, int r0, int c0) {
+    vector<vector<int>> ans;
 
-    while (i < j) {
-      ans++;
-      if (people[j] + people[i] <= limit) {
-        i++;
+    int step = 1;
+    int sign = 1;
+    ans.push_back({r0, c0});
+
+    while (true) {
+      for (int i = 0; i < step; i++) {
+        c0 += sign;
+
+        if (r0 >= 0 && r0 < R && c0 >= 0 && c0 < C) {
+          ans.push_back({r0, c0});
+        }
       }
-      j--;
-    }
-    if (i == j)
-      ans++;
+			for (int i = 0; i < step; i++) {
+        r0  += sign;
 
+        if (r0 >= 0 && r0 < R && c0 >= 0 && c0 < C) {
+          ans.push_back({r0, c0});
+        }
+      }
+			step ++;
+			sign *= -1;
+			if((int)ans.size() == R * C)
+				break;
+    }
+
+//		for(auto ele: ans){
+//			cout << ele.front() << " " << ele.back() << endl;
+//		}
     return ans;
   }
 };
-
 int main() {
-  vector<int> people = {3, 5, 3, 4};
-  int limit = 5;
+  int R = 5;
+  int C = 6;
+  int r0 = 1;
+  int c0 = 4;
   Solution solution = Solution();
-  cout << solution.numRescueBoats(people, limit);
+  solution.spiralMatrixIII(R, C, r0, c0);
 
   return 0;
 }
