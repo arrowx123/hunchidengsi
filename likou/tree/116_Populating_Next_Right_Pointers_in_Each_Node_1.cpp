b59e35ca1db43161f1070cc0a4294cb411eb521f
arrowx123 <ygfbnu@gmail.com>
2019-03-28 00:55:19 -0400

    116

---
+++ likou/tree/116_Populating_Next_Right_Pointers_in_Each_Node_1.cpp
@@ -0,0 +1,42 @@
+// iteration
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+public:
+  Node *connect(Node *root) {
+    if (!root)
+      return root;
+
+    Node *cur = nullptr;
+    Node *pre = root;
+    while (pre->left) {
+      cur = pre;
+
+      while (cur) {
+        cur->left->next = cur->right;
+        if (cur->next)
+          cur->right->next = cur->next->left;
+        cur = cur->next;
+      }
+      pre = pre->left;
+    }
+    return root;
+  }
+};
