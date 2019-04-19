ade05c9d4b25e0b7a17d0621ebe28ce20339231e
arrowx123 <ygfbnu@gmail.com>
2019-04-19 14:06:11 -0400

    560_Subarray_Sum_Equals_K

---
+++ likou/dp/560_Subarray_Sum_Equals_K.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+  int subarraySum(vector<int> &nums, int k) {
+    unordered_map<int, int> umap;
+    umap[0] = 1;
+    int sum = 0;
+    int ans = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      sum += nums[i];
+      ans += umap[sum - k];
+      umap[sum]++;
+    }
+    return ans;
+  }
+};
