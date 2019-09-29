944d0c0d997ef36a5c2f44d99e9908729255044c
arrowx123 <ygfbnu@gmail.com>
2019-09-28 23:54:45 -0400

    1000_Minimum_Cost_to_Merge_Stones

--- likou/algorithm/dp/1000_Minimum_Cost_to_Merge_Stones.cpp
+++ likou/algorithm/dp/1000_Minimum_Cost_to_Merge_Stones.cpp
@@ -24,15 +24,67 @@ public:
     for (int len = 2; len <= n; len++) {
       for (int i = 0; i + len - 1 < n; i++) {
         int j = i + len - 1;
-        for (int t = i; t < j; t++) {
-          for (int g = 2; g <= K; g++) {
+        for (int g = 2; g <= K; g++) {
+          for (int t = i; t < j; t++) {
             dp[i][j][g] = min(dp[i][j][g], dp[i][t][g - 1] + dp[t + 1][j][1]);
           }
-          dp[i][j][1] = dp[i][j][K] + prev[j + 1] - prev[i];
         }
+        dp[i][j][1] = dp[i][j][K] + prev[j + 1] - prev[i];
       }
     }
 
     return dp[0][n - 1][1] >= MAX ? -1 : dp[0][n - 1][1];
+  }
+};
+
+// O(kn^3)
+// dp top down
+const int SIZE = 31;
+const int MAX = 1e9;
+
+class Solution {
+private:
+  vector<int> stones;
+  vector<int> prev;
+  int mem[SIZE][SIZE][SIZE];
+
+  int n;
+  int K;
+
+  int dp(int l, int r, int g) {
+    if (mem[l][r][g] != -1)
+      return mem[l][r][g];
+    int &ret = mem[l][r][g];
+    ret = MAX;
+
+    if (l == r)
+      return ret;
+    if (g == 1)
+      return mem[l][r][g] = dp(l, r, K) + prev[r + 1] - prev[l];
+    for (int t = l; t < r; t++)
+      ret = min(ret, dp(l, t, g - 1) + dp(t + 1, r, 1));
+    return ret;
+  }
+
+public:
+  int mergeStones(vector<int> &stoness, int KK) {
+    stones = stoness;
+    K = KK;
+    n = stones.size();
+
+    prev.resize(n + 1);
+    for (int i = 1; i <= n; i++)
+      prev[i] = prev[i - 1] + stones[i - 1];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        for (int k = 0; k <= K; k++)
+          mem[i][j][k] = -1;
+      }
+      mem[i][i][1] = 0;
+    }
+
+    int ans = dp(0, n - 1, 1);
+    return ans >= MAX ? -1 : ans;
   }
 };
