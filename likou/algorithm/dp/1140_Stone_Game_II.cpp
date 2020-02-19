06aaf32e9f52e4a87cf33301d97261a4272f1d22
arrowx123 <ygfbnu@gmail.com>
2020-02-18 23:55:07 -0500

    1140_Stone_Game_II.cpp

--- likou/algorithm/dp/1140_Stone_Game_II.cpp
+++ likou/algorithm/dp/1140_Stone_Game_II.cpp
@@ -1,5 +1,5 @@
 // 1140_Stone_Game_II
-// dp
+// dp recursion
 class Solution {
 private:
   vector<int> sum;
@@ -31,5 +31,32 @@ public:
     for (int i = 0; i < n; i++)
       sum[i + 1] = sum[i] + piles[i];
     return find(0, 1);
+  }
+};
+
+// dp iteration
+int dp[110][220];
+int sum[110];
+
+class Solution {
+public:
+  int stoneGameII(vector<int> &piles) {
+    memset(dp, 0, sizeof(dp));
+    memset(sum, 0, sizeof(sum));
+    int n = piles.size();
+
+    for (int i = 0; i < n; i++)
+      sum[i + 1] = sum[i] + piles[i];
+
+    for (int i = n - 1; i >= 0; i--) {
+      for (int m = 1; m <= 2 * n; m++) {
+        int max_ = INT_MIN;
+        for (int x = 1; x <= 2 * m && i + x <= n; x++) {
+          max_ = max(max_, sum[n] - sum[i] - dp[i + x][max(x, m)]);
+        }
+        dp[i][m] = max_;
+      }
+    }
+    return dp[0][1];
   }
 };
